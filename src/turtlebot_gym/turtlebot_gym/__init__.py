d41bf5610c3f8df5866ac180843bcee96791b515
zzuxzt <zhanteng.xie@temple.edu>
2022-02-16 21:26:51 -0500

    Change file structure

--- src/turtlebot_gym/turtlebot_gym/__init__.py
+++
@@ -1,9 +0,0 @@
-from gym.envs.registration import register
-
-# Register drl_nav env 
-register(
-  id='drl-nav-v0',
-  entry_point='turtlebot_gym.envs:DRLNavEnv'
-  )
-
-
--- src/turtlebot_gym/turtlebot_gym/envs/__init__.py
+++
@@ -1,3 +0,0 @@
-#from turtlebot_gym.envs.turtlebot_robot_env import TurtlebotRobotEnv
-#from turtlebot_gym.envs.cnn_nav_env import CNNNavEnv
-from turtlebot_gym.envs.drl_nav_env import DRLNavEnv
