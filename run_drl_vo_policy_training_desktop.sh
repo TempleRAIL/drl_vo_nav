c7a95c92f6d4aacb112bc0815283afbc7d3a07a3
zzuxzt <zhanteng.xie@temple.edu>
2023-09-22 23:26:27 -0400

    Update training bash

--- run_drl_vo_policy_training_desktop.sh
+++ run_drl_vo_policy_training_desktop.sh
@@ -8,4 +8,9 @@ else
   echo "${DIR} not found, creating ${DIR}"
   mkdir ${DIR}
 fi
+# register gym:
+cd ./drl_vo/src/turtlebot_gym/
+pip install -e .
+cd ../../
+# roslaunch training:
 roslaunch drl_vo_nav drl_vo_nav_train.launch log_dir:="${DIR}"
--- run_drl_vo_policy_training_server.sh
+++ run_drl_vo_policy_training_server.sh
@@ -10,4 +10,9 @@ else
 fi
 Xvfb :1 -screen 0 1600x1200x16  &
 export DISPLAY=:1.0
+# register gym:
+cd ./drl_vo/src/turtlebot_gym/
+pip install -e .
+cd ../../
+# roslaunch training:
 roslaunch drl_vo_nav drl_vo_nav_train.launch gui:="false" enable_opencv:="false" enable_console_output:="false" rviz:="false" log_dir:="${DIR}"
